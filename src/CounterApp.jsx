95f2e170d95da4aa199590c385704ecf50277639
josecarreno <ing.jcarreno@gmail.com>
2022-10-09 22:44:31 -0500

    agregando button en un componente

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+    function handleAdd( event ) {
+        console.log( event );
+    }
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{ value }</h2>
+            <button onClick={ handleAdd }> +1 </button>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { CounterApp } from './CounterApp'
-import { FirstApp } from './FirstApp'
+// import { FirstApp } from './FirstApp'
 
 import './index.css'
 
@@ -10,6 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <FirstApp title="Soy Goku!"/> */}
-    <CounterApp />
+    <CounterApp value={ 2 } />
   </React.StrictMode>
 )
