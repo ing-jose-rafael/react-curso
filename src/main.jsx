4128ebe553e0b87c28a17a4668a66d5ffbde4a1a
josecarreno <ing.jcarreno@gmail.com>
2022-10-09 22:30:59 -0500

    componente counter app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { CounterApp } from './CounterApp'
 import { FirstApp } from './FirstApp'
 
 import './index.css'
@@ -8,6 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <FirstApp title="Soy Goku!"/>
+    {/* <FirstApp title="Soy Goku!"/> */}
+    <CounterApp />
   </React.StrictMode>
 )
