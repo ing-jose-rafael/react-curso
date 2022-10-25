1ec9a334a02ea370c0a169d3dc61f30015d11b95
josecarreno <ing.jcarreno@gmail.com>
2022-10-25 06:10:47 -0500

    counter app

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
    
-    const [ counter, setCounter ] = useState( 0 );
+    const [ counter, setCounter ] = useState( value );
 
     const handleAdd = ( event ) => {
-        setCounter( counter + 1 );
+        setCounter( counter + 1 ); // opt 1
+        // setCounter( (c) => c + 100 ); // opt 2
     }
+    
+    const handleReset = () => setCounter(  value ); // opt 2
+
+    const handleSubstract = () => setCounter( (c) => c - 1 )
+    
     return (
         <>
             <h1>Counter App</h1>
             <h2>{ counter }</h2>
             <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleSubstract }> -1 </button>
+            <button onClick={ handleReset }> Reset </button>
         </>
     )
 }
